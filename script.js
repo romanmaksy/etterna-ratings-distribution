da7b2021c28f6c6be49dca75ce4676e302c9a72f
Roman Maksymyschyn <contact@romanmak.com>
2021-10-02 18:39:18 +1000

    add function to select a player

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+// control binding - TODO: make some generic binder function to avoid duplicate boilerplate
+let playerHighlightInput = document.querySelector("#playerHighlightInput");
+playerHighlightInput.addEventListener("change", onPlayerHighlightChange);
+
 let binSizeInput = document.querySelector("#binSizeInput");
 binSizeInput.addEventListener("change", onBinSizeChange);
 
 let skillSetSelect = document.querySelector("#skillSetSelect");
 skillSetSelect.addEventListener("change", onSkillSetChange);
 
+let highlightedPlayerName = playerHighlightInput.value;
 let binSize = parseFloat(binSizeInput.value);
 let skill = skillSetSelect.value;
 let allRows = [];
+
+function onPlayerHighlightChange(e) {
+	highlightedPlayerName = e.target.value;
+	processData();
+}
 
 function onBinSizeChange(e) {
 	binSize = parseFloat(e.target.value);
@@ -17,6 +27,8 @@ function onSkillSetChange(e) {
 	skill = e.target.value;
 	processData();
 }
+
+// control binding end
 
 async function readCSV() {
 	SlickLoader.enable();
@@ -32,6 +44,8 @@ function processData() {
 	SlickLoader.enable();
 
 	let currentBin = binSize;
+	let scoresPerBin = [];
+	let binScoreCount = 0;
 	let scores = [];
 	let binPercentiles = [];
 
@@ -45,17 +59,41 @@ function processData() {
 	for (var i = 0; i < allScores.length; i++) {
 		let score = allScores[i];
 		scores.push(score);
+		binScoreCount++;
 
 		// add percentile text for each bin, including empty bins
 		while (parseFloat(score.toFixed(2)) > parseFloat(currentBin.toFixed(2))) {
 			binPercentiles.push(parseFloat(percentiles[i - 1]));
 			currentBin += binSize;
+			scoresPerBin.push(binScoreCount);
+			binScoreCount = 0;
 		}
 	}
 
 	// add last element
 	binPercentiles.push(100);
-	makePlotly(scores, binPercentiles);
+	scoresPerBin.push(binScoreCount);
+
+	// attempt to get info for highlighted player
+	let playerToHighlight = null;
+	if ((playerRow = allRows.find((row) => row["formatted username"] == highlightedPlayerName))) {
+		let score = parseFloat(playerRow[skill]);
+		let binIndex = Math.floor(score / binSize);
+		playerToHighlight = {
+			name: highlightedPlayerName,
+			score: score,
+			binIndex: binIndex,
+			barHeight: scoresPerBin[binIndex],
+		};
+	}
+
+	makePlotly(
+		scores,
+		binPercentiles,
+		playerToHighlight,
+		Math.max(...scores),
+		Math.max(...scoresPerBin)
+	);
 }
 
 // Returns the percentile of the given value in a sorted numeric array.
@@ -74,11 +112,13 @@ function percentRank(arr, v) {
 	return 1;
 }
 
-function makePlotly(scores, binPercentiles) {
-	var playerToHighlight = {
-		name: "miracle7",
-		rating: 15.47,
-	};
+function makePlotly(scores, binPercentiles, playerToHighlight, highestScore, highestBin) {
+	let colors = [];
+
+	for (var i = 0; i < binPercentiles.length; i++) {
+		if (playerToHighlight && i == playerToHighlight.binIndex) colors.push("rgba(100, 100, 230, 1)");
+		else colors.push("rgba(100, 100, 200, .95)");
+	}
 
 	var plotDiv = document.getElementById("plot");
 	var trace = {
@@ -92,7 +132,7 @@ function makePlotly(scores, binPercentiles) {
 		hovertemplate:
 			"Num Players: %{y}<br>" + "Rating: %{x}<br>" + "Percentile: %{text:.2f}%<extra></extra>",
 		marker: {
-			color: "rgba(100, 100, 200, .9)",
+			color: colors,
 			line: {
 				width: 1,
 			},
@@ -116,11 +156,13 @@ function makePlotly(scores, binPercentiles) {
 				text: `${skillSetSelect[skillSetSelect.selectedIndex].text} rating`,
 			},
 			dtick: 5,
+			range: [0, highestScore + highestScore * 0.15],
 		},
 		yaxis: {
 			title: {
 				text: "Player Count",
 			},
+			range: [0, highestBin + highestBin * 0.15],
 		},
 		paper_bgcolor: "rgba(0,0,0,0)",
 		plot_bgcolor: "rgba(0,0,0,0)",
@@ -129,14 +171,31 @@ function makePlotly(scores, binPercentiles) {
 		},
 	};
 
-	let histogram = Plotly.newPlot("plotlyChart", data, layout, { responsive: true });
+	if (playerToHighlight) {
+		layout.annotations = [
+			{
+				text: `user: ${playerToHighlight.name}<br>${
+					skillSetSelect[skillSetSelect.selectedIndex].text
+				} rating: ${playerToHighlight.score.toFixed(2)}`,
+				align: "left",
+				bgcolor: "rgb(20,20,20)",
+				x: playerToHighlight.score,
+				y: playerToHighlight.barHeight,
+				ax: playerToHighlight.score + binSize * 4,
+				ay: playerToHighlight.barHeight + 100 * binSize,
+				axref: "x",
+				ayref: "y",
+				arrowcolor: "rgb(200, 200, 200)",
+				font: { size: 12 },
+				borderwidth: 1,
+				arrowsize: 1,
+				arrowwidth: 1,
+			},
+		];
+	}
 
-	Promise.all([histogram]).then((values) => {
-		SlickLoader.disable();
-
-		let histogramDiv = values[0];
-		console.log(histogramDiv);
-	});
+	Plotly.newPlot("plotlyChart", data, layout, { responsive: true });
+	SlickLoader.disable();
 }
 
 readCSV();
